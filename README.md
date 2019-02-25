5459954eec56cbb25a87fcc07ef18234ca04458d
jiantong.liu <liujiantongvip@163.com>
2019-02-25 15:25:38 +0800

    tree

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # flask_demo
+
+```
+├── Pipfile
+├── Pipfile.lock
+├── README.md
+├── __pycache__
+|  ├── app.cpython-37.pyc
+|  ├── config.cpython-37.pyc
+|  └── flask_demo.cpython-37.pyc
+├── app                                 应用
+|  ├── __init__.py                      app 初始化
+|  ├── __pycache__
+|  ├── api                              api 模块
+|  ├── cms                              管理端模块
+|  ├── forms                            表单验证模块
+|  ├── libs                             工具模块
+|  ├── model                            数据库模型模块
+|  ├── secure.py                        本地配置
+|  ├── setting.py                       全局配置
+|  ├── spider                           爬虫模块
+|  ├── static                           默认静态文件
+|  ├── templates                        默认视图模板
+|  ├── view_models                      视图数据模型 （视图中所用的数据二次处理）
+|  └── web                              视图函数
+├── flask_demo.py                       入口
+└── test                                测试模块
+   ├── test.py
+   ├── test1.py
+   ├── test2.py
+   └── test3.py
+
+```
--- app/web/gift.py
+++ app/web/gift.py
@@ -2,7 +2,7 @@ from . import web
 from flask_login import login_required, current_user
 from app.model.gift import Gift
 from app.model.base import db
-from flask import current_app
+from flask import current_app, flash
 
 
 __author__ = '彤'
@@ -18,14 +18,21 @@ def my_gifts():
 @login_required
 def save_to_gifts(isbn):
     """赠送此书"""
-    gift = Gift()
-    gift.isbn = isbn
-    gift.uid = current_user.id
-    current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
+    if current_user.can_save_to_list(isbn):
+        try:
+            gift = Gift()
+            gift.isbn = isbn
+            gift.uid = current_user.id
+            current_user.beans += current_app.config['BEANS_UPLOAD_ONE_BOOK']
 
-    db.session.add(gift)
-    db.session.commit()
-    pass
+            db.session.add(gift)
+            db.session.commit()
+        except Exception as e:
+            # 数据库回滚
+            db.session.rollback()
+            raise e
+    else:
+        flash('这本书已添加至你的赠送清单或已存在与你的心愿清单，请不要重复添加')
 
 
 @web.route('/gifts/<gid>/redraw')
